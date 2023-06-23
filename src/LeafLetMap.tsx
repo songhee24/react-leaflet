81344c8a65c78b1da0f0b620b113d7df4cc15c2a
songhee24 <akaratel61@gmail.com>
2023-06-23 18:33:12 +0600

    leaflet rendering map

--- src/LeafLetMap.tsx
+++ src/LeafLetMap.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
 
 class LeafLetMap extends React.Component<any, any> {
   render() {
     return (
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+      <MapContainer
+        style={{ height: "450px", width: "100%" }}
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
