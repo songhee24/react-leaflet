cb410af234d95dd7a2f6b559dacb99153c567745
songhee24 <akaratel61@gmail.com>
2023-07-10 17:31:21 +0600

    Markers rendering

--- src/RoutingMap.tsx
+++ src/RoutingMap.tsx
@@ -37,48 +37,53 @@ const RoutingMap = () => {
       RoutingMachineRef.current = L.Routing.control({
         createMarker: function (i: any, wp: any, nWps: any) {
           if (i === 0) {
-            return L.marker(wp.latLng, {
-              icon: L.divIcon({
-                iconSize: [30, 30],
-                iconAnchor: [30 / 2.4, 30 - 16],
-                className: "mymarker",
-                html: "<img alt='img' src='https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png' />",
-              }),
-              draggable: true,
-              keyboard: true,
-              alt: "current location",
-            }).on("drag", function (e) {
-              e.latlng.alt = "current location";
-
-              console.log("there be dragons start!!", e);
-              RoutingMachineRef.handleSetMarker({
-                ...e.oldLatLng,
-                ...e.latlng,
-              });
-            });
+            return null;
+            // L.marker(wp.latLng, {
+            //     icon: L.divIcon({
+            //         iconSize: [30, 30],
+            //         iconAnchor: [30 / 2.4, 30 - 16],
+            //         className: "mymarker",
+            //         html: "<img alt='img' src='https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png' />",
+            //     }),
+            //     draggable: true,
+            //     keyboard: true,
+            //     alt: "current location",
+            // })
+            //     .on("drag", function (e) {
+            //         e.latlng.alt = "current location";
+            //
+            //         console.log("there be dragons start!!", e);
+            //         RoutingMachineRef.current.handleSetMarker({
+            //             ...e.oldLatLng,
+            //             ...e.latlng,
+            //         });
+            //     })
+            // .bindPopup(document.createElement("div").append("Hello World"));
           }
           if (i === nWps - 1) {
-            return L.marker(wp.latLng, {
-              icon: L.divIcon({
-                iconSize: [30, 30],
-                iconAnchor: [30 / 2.4, 30 - 16],
-                className: "mymarker",
-                html: "<img alt='img' src='https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png' />",
-              }),
-              draggable: true,
-              alt: "current destination",
-            })
-              .on("drag", function (e) {
-                e.latlng.alt = "current destination";
-                console.log("there be dragons dest!!", e);
-                RoutingMachineRef.handleSetMarker({
-                  ...e.oldLatLng,
-                  ...e.latlng,
-                });
-              })
-              .addEventListener("click", () => {
-                alert("hello");
-              });
+            return null;
+            //     L.marker(wp.latLng, {
+            //         icon: L.divIcon({
+            //             iconSize: [30, 30],
+            //             iconAnchor: [30 / 2.4, 30 - 16],
+            //             className: "mymarker",
+            //             html: "<img alt='img' src='https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png' />",
+            //         }),
+            //         draggable: true,
+            //         alt: "current destination",
+            //     })
+            //         .on("drag", function (e) {
+            //             e.latlng.alt = "current destination";
+            //             console.log("there be dragons dest!!", e);
+            //             RoutingMachineRef.current.handleSetMarker({
+            //                 ...e.oldLatLng,
+            //                 ...e.latlng,
+            //             });
+            //         })
+            //         .addEventListener("click", () => {
+            //             alert("hello");
+            //         });
+            // }
           }
         },
         position: "topleft",
@@ -135,20 +140,34 @@ const RoutingMap = () => {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url={maps.base}
         />
-        {/*<Marker*/}
-        {/*  icon={L.divIcon({*/}
-        {/*    iconSize: [30, 30],*/}
-        {/*    iconAnchor: [30 / 2.4, 30 - 16],*/}
-        {/*    className: "mymarker",*/}
-        {/*    html: "<img alt='img' src='https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png' />",*/}
-        {/*  })}*/}
-        {/*  // @ts-ignore*/}
-        {/*  position={start}*/}
-        {/*>*/}
-        {/*  <Popup>*/}
-        {/*    A pretty CSS3 popup. <br /> Easily customizable.*/}
-        {/*  </Popup>*/}
-        {/*</Marker>*/}
+        <Marker
+          icon={L.divIcon({
+            iconSize: [30, 30],
+            iconAnchor: [30 / 2.4, 30 - 16],
+            className: "mymarker",
+            html: "<img alt='img' src='https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png' />",
+          })}
+          // @ts-ignore
+          position={start}
+        >
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+        <Marker
+          icon={L.divIcon({
+            iconSize: [30, 30],
+            iconAnchor: [30 / 2.4, 30 - 16],
+            className: "mymarker",
+            html: "<img alt='img' src='https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png' />",
+          })}
+          // @ts-ignore
+          position={end}
+        >
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
         {/*  </LayersControl.BaseLayer>*/}
         {/*</LayersControl>*/}
       </MapContainer>
